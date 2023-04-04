9c1338fc706fa4353f8f61bf65e1928c4d56bc10
Dikshant <dikshantkhobragade12@gmail.com>
2023-04-04 15:09:36 +0530

    Header Complete

---
+++ src/components/header.jsx
@@ -0,0 +1,122 @@
+import React from 'react'
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { AppBar, Toolbar, styled, Typography, InputBase, Box } from '@mui/material';
+import Logo from '../IMAGES/Amazon-Logo.jpg';
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import Flag from '../IMAGES/Flag.jpg';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+const HeaderStyle = styled(AppBar)`
+    background:#131921;
+    
+`;
+const LogoStyle = styled('img')({
+    width: '6%',
+    cursor: 'pointer'
+})
+const DivOnestyle = styled('div')({
+    border: '2px solid transparent',
+    transition: 'border-color 0.5s ease',
+    width: '150px',
+    height: '50px',
+    margin: 0,
+    padding: 0,
+    cursor: 'pointer',
+    '&:hover': {
+        borderColor: 'white'
+    }
+});
+
+const FlagBoxStyle = styled('div')({
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: 70,
+    padding: 5,
+    marginLeft: 15,
+    height: 30,
+    cursor: 'pointer',
+    border: '2px solid transparent',
+    transition: 'border-color 0.5s ease',
+    '&:hover': {
+        borderColor: 'white'
+    }
+
+})
+
+const FlagStyle = styled('img')({
+    width: 22,
+    height: 22,
+    marginTop: 5,
+    cursor: 'pointer'
+})
+
+const SignStyle = styled('div')({
+    width: 120,
+    height: 40,
+    marginLeft: 5,
+    paddingTop: 10,
+    cursor: 'pointer',
+    border: '2px solid transparent',
+    transition: 'border-color 0.3s ease',
+    '&:hover': {
+        borderColor: 'white',
+    },
+})
+const OrderStyle = styled('div')({
+    width: 80,
+    height: 40,
+    marginLeft: 5,
+    paddingTop: 10,
+    cursor: 'pointer',
+    border: '2px solid transparent',
+    transition: 'border-color 0.3s ease',
+    '&:hover': {
+        borderColor: 'white',
+    },
+})
+
+
+
+
+const Header = () => {
+    return (
+        <div>
+            <HeaderStyle>
+                <Toolbar>
+                    <LogoStyle src={Logo} style={{ marginLeft: 10 }} />
+                    <DivOnestyle style={{ display: 'flex', marginLeft: 15 }}>
+                        <LocationOnIcon style={{ marginTop: 12 }} />
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginTop: 0 }}>
+                            <p style={{ marginBottom: 0, fontSize: 12 }}>Hello</p>
+                            <p style={{ marginBottom: 0, fontSize: 12, marginTop: 0, fontWeight: 500 }}>Select your address</p>
+                        </div>
+                    </DivOnestyle>
+                    <InputBase style={{ backgroundColor: 'whitesmoke', width: '50%', padding: 5, borderRadius: 6, marginLeft: 5 }}
+                        placeholder='Search Amazon.in'
+                        startAdornment={<>
+                            <Typography variant='body2'>All</Typography>
+                            <ArrowDropDownIcon />
+                        </>}
+                        endAdornment={<SearchIcon />} />
+                    <FlagBoxStyle >
+                        <FlagStyle src={Flag} />
+                        <h4 style={{ display: 'flex', marginTop: 5 }}>EN <ArrowDropDownIcon /></h4>
+                    </FlagBoxStyle>
+                    <SignStyle >
+                        <p style={{ marginTop: 0, marginRight: 20, marginBottom: 0, fontSize: 12 }}>Hello,sign in </p>
+                        <p style={{ marginTop: 0, marginRight: 10, marginBottom: 0, fontSize: 12 }}>Account & List<ArrowDropDownIcon style={{ marginLeft: 2, fontSize: 10 }} /></p>
+                    </SignStyle>
+                    <OrderStyle >
+                        <p style={{ marginTop: 0, marginRight: 10, marginBottom: 0, fontSize: 12 }}>Returns & </p>
+                        <p style={{ marginTop: 0, marginRight: 10, marginBottom: 0, fontSize: 12 }}>Orders<ArrowDropDownIcon style={{ marginLeft: 2, fontSize: 10 }} /></p>
+                    </OrderStyle>
+                    <ShoppingCartIcon style={{ fontSize: 30, marginLeft: 20, cursor: 'pointer' }} />
+                </Toolbar>
+            </HeaderStyle>
+
+        </div>
+    )
+}
+
+export default Header
