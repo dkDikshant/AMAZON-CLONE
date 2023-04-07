b61a5683cdc532fbcf04d6be03903e0960172fa2
Dikshant <dikshantkhobragade12@gmail.com>
2023-04-07 15:09:15 +0530

    Carousel BOX Done

--- src/components/Slides.jsx
+++ src/components/Slides.jsx
@@ -8,8 +8,14 @@ import four from '../IMAGES/four.jpg'
 import five from '../IMAGES/five.jpg'
 import six from '../IMAGES/six.jpg'
 import styled from '@emotion/styled';
-import SlidesBoxes from './SlidesBoxes';
-
+import phar from '../IMAGES/pharmacy.jpg'
+import { Box, Typography, Button } from '@mui/material';
+import img1 from '../IMAGES/img1.jpg'
+import img2 from '../IMAGES/img2.jpg'
+import img3 from '../IMAGES/img3.jpg'
+import img4 from '../IMAGES/img4.jpg'
+import laptop from '../IMAGES/laptop.jpg'
+import laptop1 from '../IMAGES/laptop1.jpg'
 
 const responsive = {
 
@@ -28,7 +34,9 @@ const responsive = {
 };
 
 const SliderStyle = styled('img')({
-    width: '100%'
+    width: '100%',
+    background: 'linear-gradient(to bottom ,transparent,rgba(0,0,0,0.7))'
+
 })
 const StyledCarousel = styled(Carousel)`
   .react-multiple-carousel__arrow--left,
@@ -38,27 +46,99 @@ const StyledCarousel = styled(Carousel)`
   
 `;
 
+const Container = styled('div')({
+    position: 'relative'
+})
+
+const BoxStyle = styled(Box)({
+    position: 'absolute',
+    bottom: -200,
+    display: 'flex',
+    justifyContent: 'space-between',
+    left: '2%'
+})
+const InsideBoxStyle = styled(Box)({
+    marginTop: '20%',
+    width: '300px',
+    height: '420px',
+    background: '#fff',
+    boxShadow: ' rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px',
+    marginBottom: 20
+})
 const Slides = () => {
     return (
-        <div>
+        <Container>
             <StyledCarousel
                 swipeable={false}
                 responsive={responsive}
                 draggable={false}
                 infinite={true}
                 autoPlay={true}
-                autoPlaySpeed={1000}
-
+                autoPlaySpeed={3000}
             >
-                <div><SliderStyle src={one} /></div>
+                <div><SliderStyle src={one} style={{ background: 'linear-gradient(to bottom, transparent, rgba(0,0,0,0.7))' }} /></div>
                 <div><SliderStyle src={two} /></div>
                 <div><SliderStyle src={three} /></div>
                 <div><SliderStyle src={four} /></div>
                 <div><SliderStyle src={five} /></div>
                 <div><SliderStyle src={six} /></div>
             </StyledCarousel>
-            <SlidesBoxes />
-        </div>
+            <BoxStyle>
+                <InsideBoxStyle>
+                    <Typography variant='h5' style={{ padding: 10, fontWeight: 800 }}>
+                        Up to 50% off | Monitor blood sugar at home
+                    </Typography>
+                    <img src={phar} style={{ width: '90%', height: '250px', padding: '10px' }} />
+                    <a href="#" style={{ textDecoration: 'none', padding: 10 }}>Visit the store </a>
+                </InsideBoxStyle>
+                <InsideBoxStyle style={{ marginLeft: '10px' }}>
+                    <Typography variant='h5' style={{ padding: 10, fontWeight: 800 }}>
+                        Shop & earn rewards up to ₹500 on intimate...
+                    </Typography>
+                    <Box style={{ display: 'flex' }}>
+                        <Box style={{ width: 130, height: 150, padding: 5, marginLeft: 10 }}>
+                            <img src={img1} style={{ width: 130, height: 100, marginBottom: 0, paddingBottom: 0 }} />
+                            <p style={{ fontSize: 12, paddingTop: 0, marginTop: 0, fontWeight: 400, paddingLeft: 10 }}>Minimum Order of {"\u20B9"} 1799</p>
+                        </Box>
+                        <Box style={{ width: 130, height: 150, padding: 5, marginRight: 10 }}>
+                            <img src={img2} style={{ width: 130, height: 100 }} />
+                            <p style={{ fontSize: 12, paddingTop: 0, marginTop: 0, fontWeight: 400, paddingLeft: 10 }}>Minimum Order of {"\u20B9"} 750</p>
+                        </Box>
+                    </Box>
+                    <Box style={{ display: 'flex' }}>
+                        <Box style={{ width: 130, height: 150, padding: 5, marginLeft: 10 }}>
+                            <img src={img3} style={{ width: 130, height: 100, marginBottom: 0, paddingBottom: 0 }} />
+                            <p style={{ fontSize: 12, paddingTop: 0, marginTop: 0, fontWeight: 400, paddingLeft: 10 }}>Minimum Order of {"\u20B9"} 999</p>
+                        </Box>
+                        <Box style={{ width: 130, height: 150, padding: 5, marginRight: 10 }}>
+                            <img src={img4} style={{ width: 130, height: 100 }} />
+                            <p style={{ fontSize: 12, paddingTop: 0, marginTop: 0, fontWeight: 400, paddingLeft: 10 }}>Minimum Order of {"\u20B9"} 799</p>
+                        </Box>
+                    </Box>
+                </InsideBoxStyle>
+                <InsideBoxStyle style={{ marginLeft: '10px' }}>
+                    <Typography variant='h5' style={{ padding: 10, fontWeight: 800 }}>
+                        Up to 50% off | Monitor blood sugar at home
+                    </Typography>
+                    <img src={laptop} style={{ width: '90%', height: '250px', padding: '10px' }} />
+                    <a href="#" style={{ textDecoration: 'none', padding: 10 }}>See more </a>
+
+                </InsideBoxStyle>
+                <InsideBoxStyle style={{ marginLeft: '10px' }}>
+                    <Box style={{ width: 300, height: 150, boxShadow: ' rgba(0, 0, 0, 0.15) 0px 15px 25px, rgba(0, 0, 0, 0.05) 0px 5px 10px' }}>
+                        <Typography style={{ paddingTop: 20, paddingLeft: 50, paddingRight: 30, fontWeight: 800 }}>Sign In for Your Best Experience</Typography>
+                        <Button style={{ background: '#FFD814', marginLeft: 30, paddingLeft: 50, paddingRight: 50, marginTop: 10 }}>Sign in securly</Button>
+                    </Box>
+                    <Box style={{ marginTop: 30, marginLeft: 10 }}>
+                        <img src={laptop1} style={{ height: 230 }} />
+
+                    </Box>
+                </InsideBoxStyle>
+
+            </BoxStyle>
+
+
+        </Container>
     )
 }
 
